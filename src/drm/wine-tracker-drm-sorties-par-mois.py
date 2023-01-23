174183c4a4824601b72971a82983e29b86401ba2
Honorine-MC <honorine.mariechristy@gmail.com>
2023-01-23 14:19:40 +0100

    graphe 3 pour tous les opérateurs

--- src/drm/wine-tracker-drm-sorties-par-mois.py
+++ src/drm/wine-tracker-drm-sorties-par-mois.py
@@ -12,25 +12,29 @@ import argparse
 # In[ ]:
 
 
-#arguments
-id_operateur="XXXXX-XX"
+dossier_graphes="../../graphes/"
+csv = "../../data/drm/export_bi_mouvements.csv"
+source = "DRM Inter-Rhône"
 
-parser = argparse.ArgumentParser()
-parser.add_argument("id_operateur", help="Identifiant opérateur", default=id_operateur, nargs='?')
-
-try:
-    args = parser.parse_args()
-    id_operateur = args.id_operateur
-except:
-    print("Arguments pas défaut")
+mois = { "08" : "Août" , "09" : "Septembre", "10" : "Octobre", "11" : "Novembre" , "12" : "Décembre",
+        "01" : "Janvier", "02" : "Février", "03" : "Mars", "04" : "Avril", "05" : "Mai", "06" : "Juin",
+        "07" : "Juillet" }
 
 
 # In[ ]:
 
 
-dossier_graphes="../../graphes/"
-csv = "../../data/drm/export_bi_mouvements.csv"
-source = "DRM Inter-Rhône"
+#arguments
+id_operateur=None
+
+parser = argparse.ArgumentParser()
+parser.add_argument("id_operateur", help="Identifiant opérateur", default=id_operateur, nargs='?')
+
+try:
+   args = parser.parse_args()
+   id_operateur = args.id_operateur
+except:
+   print("Arguments pas défaut")
 
 
 # In[ ]:
@@ -38,53 +42,50 @@ source = "DRM Inter-Rhône"
 
 #préparations des données de l'opérateur sans filtres
 mouvements = pd.read_csv(csv, sep=";",encoding="iso8859_15", low_memory=False)
-mouvements.rename(columns = {'identifiant declarant':'identifiant_declarant'}, inplace = True)
-mouvements = mouvements.query("identifiant_declarant == @id_operateur").reset_index()
 mouvements["volume mouvement"] = mouvements["volume mouvement"]*(-1)
+mouvements.rename(columns = {'identifiant declarant':'identifiant_declarant'}, inplace = True)
 
 
 # In[ ]:
 
 
-#applications des filtres 
-mouvements['sorties'] = mouvements["type de mouvement"].str.lower().str.startswith("sorties/")
-sorties = mouvements.query("sorties == True")
+def create_graph(id_operateur,mouvements):
+    mouvements['sorties'] = mouvements["type de mouvement"].str.lower().str.startswith("sorties/")
+    sorties = mouvements.query("sorties == True")
 
-sorties = sorties.groupby(['campagne', 'periode'])['volume mouvement'].sum().reset_index()
-sorties['mois'] = sorties["periode"].str.extract('.*(\d{2})', expand = False)
+    sorties = sorties.groupby(['campagne', 'periode'])['volume mouvement'].sum().reset_index()
+    sorties['mois'] = sorties["periode"].str.extract('.*(\d{2})', expand = False)
+    sorties['mois'] = sorties['mois'].map(mois,na_action=None)
 
-mois = { "08" : "Août" , "09" : "Septembre", "10" : "Octobre", "11" : "Novembre" , "12" : "Décembre",
-        "01" : "Janvier", "02" : "Février", "03" : "Mars", "04" : "Avril", "05" : "Mai", "06" : "Juin", 
-        "07" : "Juillet" }
+    # CREATION DU GRAPHE
+    fig = px.histogram(sorties, x="mois", y="volume mouvement",
+                 color='campagne', barmode='group',
+                 height=500)
+    fig.update_layout(xaxis_title=None,
+                      yaxis_title=None,
+                      legend_title=None,
+                      paper_bgcolor="#b7e1e5",
+                      plot_bgcolor = "white",
+                      yaxis=dict(tickformat=".f"),
+                      legend=dict(orientation="h",xanchor = "center",x = 0.5)
+                     )
+    fig.for_each_xaxis(lambda x: x.update(showgrid=False))
+    fig.for_each_yaxis(lambda x: x.update(gridcolor='Lightgrey'))
+    fig.update_xaxes(fixedrange=True)
+    fig.update_yaxes(fixedrange=True)
+    fig.show()
 
-sorties['mois'] = sorties['mois'].map(mois,na_action=None)
-
-
-# In[ ]:
-
-
-# CREATION DU GRAPHE
-fig = px.histogram(sorties, x="mois", y="volume mouvement",
-             color='campagne', barmode='group',
-             height=500)
-fig.update_layout(xaxis_title=None, 
-                  yaxis_title=None,
-                  legend_title=None,
-                  paper_bgcolor="#b7e1e5",
-                  plot_bgcolor = "white",
-                  yaxis=dict(tickformat=".f"),                  
-                  legend=dict(orientation="h",xanchor = "center",x = 0.5)
-                 )
-fig.for_each_xaxis(lambda x: x.update(showgrid=False))
-fig.for_each_yaxis(lambda x: x.update(gridcolor='Lightgrey'))
-fig.update_xaxes(fixedrange=True)
-fig.update_yaxes(fixedrange=True)
-fig.show()
+    # GRAPHE DANS UN FICHIER HTML
+    fig.write_html(dossier_graphes+id_operateur+"_graphe3.html")
+    return
 
 
 # In[ ]:
 
 
-# GRAPHE DANS UN FICHIER HTML
-fig.write_html(dossier_graphes+id_operateur+"_graphe3.html")
+if(id_operateur):
+    create_graph(id_operateur,mouvements)
+else :
+    for identifiant in mouvements.identifiant_declarant.unique():
+        create_graph(identifiant,mouvements)
 
