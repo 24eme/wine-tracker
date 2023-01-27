76698166d083bd91713640e7f9664264cf1b0dc9
Honorine-MC <honorine.mariechristy@gmail.com>
2023-01-27 10:52:29 +0100

    script generate_all fonctionneaussi pour un id donné

--- src/drm/generate_all.sh
+++ src/drm/generate_all.sh
@@ -2,6 +2,17 @@
 
 mkdir -p graphes
 
-python3 src/drm/wine-tracker-drm-stock-recoltes-sorties.py
-python3 src/drm/wine-tracker-drm-sortie_vrac_condionne.py
-python3 src/drm/wine-tracker-drm-sorties-par-mois.py
+
+if test "$1" ; then
+	cvi=$1
+	echo "$cvi"
+	python3 src/drm/wine-tracker-drm-stock-recoltes-sorties.py $cvi
+        python3 src/drm/wine-tracker-drm-sortie-vrac-condionne.py $cvi
+        python3 src/drm/wine-tracker-drm-sorties-par-mois.py $cvi
+	python3 src/drm/wine-tracker-drm-sortie-tous.py $cvi
+else
+	echo "ICI"
+	python3 src/drm/wine-tracker-drm-stock-recoltes-sorties.py
+	python3 src/drm/wine-tracker-drm-sortie-vrac-condionne.py
+	python3 src/drm/wine-tracker-drm-sorties-par-mois.py
+fi
