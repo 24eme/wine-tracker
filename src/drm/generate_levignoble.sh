98961968ccfe5690c3d112cf2f28bd289ce97f13
Honorine-MC <honorine.mariechristy@gmail.com>
2023-02-23 16:25:03 +0100

    graphe 4 sortie par trimestre pour le vignoble

--- src/drm/generate_levignoble.sh
+++ src/drm/generate_levignoble.sh
@@ -1,3 +1,4 @@
 python3 src/drm/wine-tracker-drm-stock-recoltes-sorties-moyenne.py
 python3 src/drm/wine-tracker-drm-sortie-vrac-condionne-moyenne.py
 python3 src/drm/wine-tracker-drm-sortie-tous-moyenne.py
+python3 src/drm/wine-tracker-drm-sorties-par-trimestre-moyenne.py
