497e3548232e0eb703cbf56f68c49ecc5e151eb5
Gabriel Poma <gpoma@24eme.fr>
2023-03-01 12:59:50 +0100

    shebang sh

--- src/drm/generate_levignoble.sh
+++ src/drm/generate_levignoble.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 python3 src/drm/wine-tracker-drm-stock-recoltes-sorties-moyenne.py
 python3 src/drm/wine-tracker-drm-sortie-vrac-condionne-moyenne.py
 python3 src/drm/wine-tracker-drm-sortie-tous-moyenne.py
