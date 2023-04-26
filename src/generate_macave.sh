86950382c434d5f9493fe8ac84c4d067ad2cca6f
Tangui Morlier <tangui@tangui.eu.org>
2023-04-26 11:32:33 +0200

    L'argument attendu n'est pas un CVI + simplification du code en évitant de la duplication

--- src/generate_macave.sh
+++ src/generate_macave.sh
@@ -1,31 +1,24 @@
 #!/bin/bash
 
-CVI=
-[ $# -eq 1 ] && CVI=$1
+IDENTIFIANT=$1
 
-if [ -n "$CVI" ]; then echo "$CVI"; else echo "TOUS"; fi
+if [ -n "$IDENTIFIANT" ]; then echo "$IDENTIFIANT"; else echo "TOUS"; fi
 
-python3 src/drm-stock-recoltes-sorties.py "$CVI"
-python3 src/drm-sortie-vrac-condionne.py "$CVI"
-python3 src/drm-sortie-tous.py "$CVI"
-python3 src/drm-sorties-cumul-par-mois.py "$CVI"
-python3 src/drm-sorties-par-campagne-et-mois.py "$CVI"
+python3 src/drm-stock-recoltes-sorties.py "$IDENTIFIANT"
+python3 src/drm-sortie-vrac-condionne.py "$IDENTIFIANT"
+python3 src/drm-sortie-tous.py "$IDENTIFIANT"
+python3 src/drm-sorties-cumul-par-mois.py "$IDENTIFIANT"
+python3 src/drm-sorties-par-campagne-et-mois.py "$IDENTIFIANT"
 
-if [ -n "$CVI" ];then
-  python3 src/contrats-contractualisation-mes-clients.py "$CVI"
-  python3 src/contrats-contractualisation-mes-clients-tableau-a-date.py "$CVI"
-  python3 src/contrats-contractualisation-comparaison-deroulement-par-campagne.py "$CVI"
-  python3 src/contrats-contractualisation-top10-5-dernieres-campagnes.py "$CVI"
-  python3 src/informations-operateur.py "$CVI"
-  python3 src/chiffres-cles.py "$CVI"
+if [ -n "$IDENTIFIANT" ];then
+    echo $IDENTIFIANT
 else
-  cut -d ';' -f7 < data/contrats/export_bi_etablissements.csv | while read ID;
-  do
+    tail -n +2 data/contrats/export_bi_etablissements.csv | cut -d ';' -f7
+fi | while read ID; do
     python3 src/contrats-contractualisation-mes-clients.py "$ID"
     python3 src/contrats-contractualisation-mes-clients-tableau-a-date.py "$ID"
     python3 src/contrats-contractualisation-comparaison-deroulement-par-campagne.py "$ID"
     python3 src/contrats-contractualisation-top10-5-dernieres-campagnes.py "$ID"
     python3 src/informations-operateur.py "$ID"
     python3 src/chiffres-cles.py "$ID"
-  done
-fi
+done
