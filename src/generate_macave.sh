f6c19080aa9a81a1fc81d45dc29d61714bed130b
Tangui Morlier <tangui@tangui.eu.org>
2023-05-02 20:41:27 +0200

    On commence pas les chiffres clés pour avoir des choses à afficher même si c'est partiel

--- src/generate_macave.sh
+++ src/generate_macave.sh
@@ -4,6 +4,17 @@ IDENTIFIANT=$1
 
 if [ -n "$IDENTIFIANT" ]; then echo "$IDENTIFIANT"; else echo "TOUS"; fi
 
+
+if [ -n "$IDENTIFIANT" ];then
+    echo $IDENTIFIANT
+else
+    tail -n +2 data/contrats/export_bi_etablissements.csv | cut -d ';' -f7
+fi | while read ID; do
+    python3 src/00_prealable/01_chiffres-cles.py "$ID"
+    python3 src/00_prealable/00_informations-operateur.py "$ID"
+done
+
+
 python3 src/01_DRM/01_drm-stock-recoltes-sorties.py "$IDENTIFIANT"
 python3 src/01_DRM/02_drm-sortie-vrac-condionne.py "$IDENTIFIANT"
 python3 src/01_DRM/03_drm-sorties-par-campagne-et-mois.py "$IDENTIFIANT"
@@ -19,7 +30,4 @@ fi | while read ID; do
     python3 src/02_contrat/01_contrats-contractualisation-mes-clients-tableau-a-date.py "$ID"
     python3 src/02_contrat/02_contrats-contractualisation-top10-5-dernieres-campagnes.py "$ID"
     python3 src/02_contrat/03_contrats-contractualisation-comparaison-deroulement-par-campagne.py "$ID"
-
-    python3 src/00_prealable/01_chiffres-cles.py "$ID"
-    python3 src/00_prealable/00_informations-operateur.py "$ID"
 done
