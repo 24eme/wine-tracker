c6f5122221a80e10f86e680d98a5d665f2803879
Honorine-MC <honorine.mariechristy@gmail.com>
2023-05-03 11:25:08 +0200

    appel d'un script qui a été supprimé

--- src/generate_levignoble.sh
+++ src/generate_levignoble.sh
@@ -3,5 +3,4 @@
 python3 src/01_DRM/01_drm-stock-recoltes-sorties-le-vignoble.py
 python3 src/01_DRM/02_drm-sortie-vrac-condionne-le-vignoble.py
 python3 src/01_DRM/03_drm-sorties-par-campagne-et-mois-le-vignoble.py
-python3 src/01_DRM/drm-sorties-par-trimestre-le-vignoble.py
 python3 src/01_DRM/drm-sortie-tous-le-vignoble.py
