46820b47770f04d5b18acf5cddbb08581d41864f
Tangui Morlier <tangui@tangui.eu.org>
2023-05-04 00:27:53 +0200

    Possibilité de générer tous les scripts préalable sans id d'opérateur

--- src/generate_macave.sh
+++ src/generate_macave.sh
@@ -5,15 +5,8 @@ IDENTIFIANT=$1
 if [ -n "$IDENTIFIANT" ]; then echo "$IDENTIFIANT"; else echo "TOUS"; fi
 
 
-if [ -n "$IDENTIFIANT" ];then
-    echo $IDENTIFIANT
-else
-    tail -n +2 data/contrats/export_bi_etablissements.csv | cut -d ';' -f7
-fi | while read ID; do
-    python3 src/00_prealable/01_chiffres-cles.py "$ID"
-    python3 src/00_prealable/00_informations-operateur.py "$ID"
-done
-
+python3 src/00_prealable/01_chiffres-cles.py "$IDENTIFIANT"
+python3 src/00_prealable/00_informations-operateur.py "$IDENTIFIANT"
 
 python3 src/01_DRM/01_drm-stock-recoltes-sorties.py "$IDENTIFIANT"
 python3 src/01_DRM/02_drm-sortie-vrac-condionne.py "$IDENTIFIANT"
